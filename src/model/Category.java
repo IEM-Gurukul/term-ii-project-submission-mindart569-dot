6114b2cceccefabcaee6319cae475b64097d951a
mindart569-dot <mindart569@gmail.com>
2026-03-21 00:19:14 +0530

    Add Category enum with expense categories and printAll method

---
+++ src/model/Category.java
@@ -0,0 +1,24 @@
+package model;
+
+/**
+ * Enum representing expense categories.
+ * Using enum ensures only valid categories can be used.
+ */
+public enum Category {
+    FOOD,
+    TRANSPORT,
+    EDUCATION,
+    ENTERTAINMENT,
+    HEALTH,
+    OTHER;
+
+    /**
+     * Displays categories in a numbered list for the user.
+     */
+    public static void printAll() {
+        Category[] categories = Category.values();
+        for (int i = 0; i < categories.length; i++) {
+            System.out.println((i + 1) + ". " + categories[i]);
+        }
+    }
+}s
